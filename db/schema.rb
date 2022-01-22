902672658e0267e4becbdb2f99d51c5035eb17be
生澤樹 <ikuzawatatsuki@ikuzawajunoMacBook-Pro.local>
2022-01-23 06:54:57 +0900

    category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_175116) do
+ActiveRecord::Schema.define(version: 2022_01_22_212958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "major_category_name"
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+major_category_names = ["夏のスポーツ", "冬のスポーツ"]
+
+summer_sports_categories = ["野球", "サッカー", "水泳", "テニス", "ゴルフ", "バスケットボール", "柔道", "レスリング", "体操", "ラグビー", "アメフト", "スケートボード", "卓球"]
+winter_sports_categories = ["フィギュアスケート", "スキー・スノーボード", "カーリング", "その他"]
+
+major_category_names.each do |major_category_name|
+  if major_category_name == "夏のスポーツ"
+    summer_sports_categories.each do |summer_sports_category|
+      Category.create( 
+        name: summer_sports_category, 
+        description: summer_sports_category, 
+        major_category_name: major_category_name 
+      ) 
+    end
+  elsif major_category_name == "冬のスポーツ"
+    winter_sports_categories.each do |winter_sports_category|
+      Category.create( 
+        name: winter_sports_category, 
+        description: winter_sports_category, 
+        major_category_name: major_category_name 
+      ) 
+    end
+  end
+end
