cb2f7d70504e3b741d3cd10b89984240465effe1
tastupen <tatsupen0828@au.com>
2022-02-05 18:18:22 +0900

    miss

--- Gemfile
+++ Gemfile
@@ -41,6 +41,9 @@ gem 'dotenv-rails'
 
 gem 'kaminari'
 
+#fontawesome
+gem 'font-awesome-sass'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => ENV["GMAIL_USERNAME"] ,
+    :user_name => ENV["GMAIL_ADDRESS"] ,
     :password => ENV["GMAIL_PASSWORD"] ,
     :authentication => :plain,
     :enable_starttls_auto => true
