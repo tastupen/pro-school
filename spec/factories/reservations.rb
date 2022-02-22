bb6dfda72649285cccbe78c3f6d229d2ac78ba7f
tastupen <tatsupen0828@au.com>
2022-02-23 00:21:01 +0900

    rspec test file chenge

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :reservation do
-    start_date { "2022-02-12 01:52:34" }
+    association :user
+    association :lesson
+    association :admin
+    start_time { "2022-02-12 01:52:34" }
   end
 end
