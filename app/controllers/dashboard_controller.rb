7b0f626abb603bf6f640408e4e427c258cb8be45
tastupen <tatsupen0828@au.com>
2022-03-08 01:05:42 +0900

    admin_lesson_ranking

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,5 +5,6 @@ class DashboardController < ApplicationController
   def index
     @admin = current_admin
     @lessons = current_admin.lessons.limit(3)
+    @lesson_ranking = current_admin.lessons.find(Like.group(:lesson_id).order('count(lesson_id) desc').pluck(:lesson_id))
   end
 end
