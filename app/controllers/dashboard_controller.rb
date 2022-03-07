d6cbc565bb48bd67d6a9485ebf838922bf57d258
tastupen <tatsupen0828@au.com>
2022-03-08 01:18:30 +0900

    comment_ranking

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,5 +6,6 @@ class DashboardController < ApplicationController
     @admin = current_admin
     @lessons = current_admin.lessons.limit(3)
     @lesson_ranking = current_admin.lessons.find(Like.group(:lesson_id).order('count(lesson_id) desc').pluck(:lesson_id))
+    @lesson_comment_rank = current_admin.lessons.find(Comment.group(:lesson_id).order('count(lesson_id) desc').pluck(:lesson_id))
   end
 end
